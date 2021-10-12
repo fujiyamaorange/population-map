ab23b4447abcc559255e1ca5cb8276683c33de3e
FujimuraKaito <kaito.bakuman@gmail.com>
2021-10-13 08:47:31 +0900

    🥅 use try catch syntax

--- src/utils/api/getPrefData.ts
+++ src/utils/api/getPrefData.ts
@@ -4,17 +4,21 @@
  * 都道府県コードから人口データを取得する
  */
 const getPrefData = async (prefCode: number): Promise<any> => {
-  const url = `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=${prefCode}&cityCode=-`;
-  const res = await fetch(url, {
-    method: 'GET',
-    headers: {
-      'X-API-KEY': `${process.env.REACT_APP_API_KEY}`,
-    },
-  });
-  if (!res.ok) {
+  try {
+    const url = `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=${prefCode}&cityCode=-`;
+    const res = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'X-API-KEY': `${process.env.REACT_APP_API_KEY}`,
+      },
+    });
+    if (!res.ok) {
+      return 'error';
+    }
+    return res.json();
+  } catch (e) {
     return 'error';
   }
-  return res.json();
 };
 
 export default getPrefData;
